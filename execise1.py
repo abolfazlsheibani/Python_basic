5b5889bf324d2098c9fd91df5f70012b349b54ee
Abolfazl Sheibani <abolfazl2886174@gmail.com>
2026-06-21 09:04:15 +0330

    Create execise1.py

---
+++ execise1.py
@@ -0,0 +1,10 @@
+# لیستی با نام numbers داریم. عنصر اول و آخر لیست numbers را چاپ کنید.
+# numbers = [5, 9, 3, 0, 7, 1]
+
+
+# {پاسخ}
+
+numbers = [5, 9, 3, 0, 7, 1]
+a=numbers[0]
+b=numbers[5]
+print(a,"and",b)
